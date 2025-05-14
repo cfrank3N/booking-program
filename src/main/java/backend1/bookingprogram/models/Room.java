2e73680638fc0b3fbf0ac100246ed4a58390ded9
cfrank3N <adamcecarfrank@gmail.com>
2025-05-14 14:01:52 +0200

    Added validation to room number

--- src/main/java/backend1/bookingprogram/models/Room.java
+++ src/main/java/backend1/bookingprogram/models/Room.java
@@ -2,6 +2,7 @@ package backend1.bookingprogram.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class Room {
     @GeneratedValue
     private Long id;
     @NotEmpty
+    @Pattern(regexp = "^[1-9][0-9][1-9]$", message = "Room number must be 3 digits starting with 1-9, and the last digit cannot be 0.")
     private String roomNumber;
     private String roomName;
     @NotEmpty
