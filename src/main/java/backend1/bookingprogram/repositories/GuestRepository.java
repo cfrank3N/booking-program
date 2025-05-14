38afda95eadeeb41151290afda05a866d829b5ad
cfrank3N <adamcecarfrank@gmail.com>
2025-05-14 12:12:54 +0200

    Added JPA repo's to all POJOS

---
+++ src/main/java/backend1/bookingprogram/repositories/GuestRepository.java
@@ -0,0 +1,6 @@
+package backend1.bookingprogram.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class GuestRepository implements JpaRepository<Guest, Long> {
+}
---
+++ src/main/java/backend1/bookingprogram/repositories/RoomRepository.java
@@ -0,0 +1,6 @@
+package backend1.bookingprogram.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class RoomRepository implements JpaRepository<Room, Long> {
+}
