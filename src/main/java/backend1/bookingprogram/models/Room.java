4c0e6b140fa45b4729e4051651a4cc71e08f64a9
ArvidUtas <arvidu@hotmail.com>
2025-05-14 12:02:49 +0200

    initial commit

---
+++ src/main/java/backend1/bookingprogram/models/Room.java
@@ -0,0 +1,18 @@
+package backend1.bookingprogram.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+@Setter
+@Getter
+public class Room {
+    @Id
+    @GeneratedValue
+    Long id;
+}
