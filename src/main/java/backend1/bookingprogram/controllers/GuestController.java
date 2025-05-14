3a82b9a6066687f2110aece9b0c3055365377d3a
cfrank3N <adamcecarfrank@gmail.com>
2025-05-14 17:25:02 +0200

    Can add guest. Get's good error message if request is faulty. Added exceptionHandler

--- src/main/java/backend1/bookingprogram/controllers/GuestController.java
+++ src/main/java/backend1/bookingprogram/controllers/GuestController.java
@@ -1,17 +1,25 @@
 package backend1.bookingprogram.controllers;
 
-import backend1.bookingprogram.repositories.GuestRepository;
+import backend1.bookingprogram.models.Guest;
+import backend1.bookingprogram.service.BookingService;
+import jakarta.validation.Valid;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuestController {
 
-    private final GuestRepository guestRepository;
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(GuestController.class);
 
-    public GuestController(GuestRepository guestRepository) {
-        this.guestRepository = guestRepository;
-    }
+    @Autowired
+    private BookingService bookingService;
 
+    @PostMapping("/guest")
+    public ResponseEntity<String> createGuest(@Valid @RequestBody Guest g) {
+        return bookingService.createGuest(g);
+    }
 }
