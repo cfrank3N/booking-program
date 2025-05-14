acc9aa437abc93cd4783d7345783bd01d8d68005
cfrank3N <adamcecarfrank@gmail.com>
2025-05-14 11:15:59 +0200

    First commit

---
+++ src/main/java/backend1/bookingprogram/BookingProgramApplication.java
@@ -0,0 +1,13 @@
+package backend1.bookingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend1/bookingprogram/BookingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package backend1.bookingprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
