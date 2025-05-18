037dcc7aad8db19a9f1e53a9c56ef94a7c556ac1
Gaurgle <justwannagroove@gmail.com>
2025-05-18 17:40:50 +0200

    db seeder post construct

--- src/main/java/backend1/bookingprogram/DatabaseSeeder.java
+++ src/main/java/backend1/bookingprogram/DatabaseSeeder.java
@@ -1,6 +1,10 @@
 package backend1.bookingprogram;
 
+import backend1.bookingprogram.models.Booking;
+import backend1.bookingprogram.models.Guest;
 import backend1.bookingprogram.models.Room;
+import backend1.bookingprogram.repositories.BookingRepository;
+import backend1.bookingprogram.repositories.GuestRepository;
 import backend1.bookingprogram.repositories.RoomRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
@@ -12,20 +16,37 @@ import java.util.List;
 public class DatabaseSeeder {
 
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final BookingRepository bookingRepository;
 
-    public DatabaseSeeder(RoomRepository roomRepository) {
+    public DatabaseSeeder(RoomRepository roomRepository, GuestRepository guestRepository, BookingRepository bookingRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.bookingRepository = bookingRepository;
     }
 
-
-    // example
+    // seeds db
     @PostConstruct
     public void seed() {
         if (roomRepository.count() == 0) {
             List<Room> rooms = List.of(
                     new Room(null, "101", "single room", 12, null));
-                    roomRepository.saveAll(rooms);
+            roomRepository.saveAll(rooms);
+        }
 
+        if (guestRepository.count() == 0) {
+            List<Guest> guests = List.of(
+                    new Guest(null, "Andreas", "Andreas@Hotmale.com", "+46763060692", null));
+            guestRepository.saveAll(guests);
+        }
+
+        if (bookingRepository.count() == 0) {
+            List<Booking> bookings = List.of(
+                    new Booking());
+            bookingRepository.saveAll(bookings);
+            // osv.
         }
     }
+
+
 }
