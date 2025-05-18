b863b7701a45b7e3192ed098a74bc997ace9af80
Gaurgle <justwannagroove@gmail.com>
2025-05-19 00:23:49 +0200

    wrong annotations cause crash, wip

--- src/main/java/backend1/bookingprogram/models/Room.java
+++ src/main/java/backend1/bookingprogram/models/Room.java
@@ -1,6 +1,7 @@
 package backend1.bookingprogram.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
@@ -21,15 +22,16 @@ public class Room {
     @Pattern(regexp = "^[1-9][0-9][1-9]$", message = "Room number must be 3 digits starting with 1-9, and the last digit cannot be 0.")
     private String roomNumber;
     private String roomName;
-    @NotEmpty
-    private int size;
+    @Min(value = 1, message ="Room size can't be smaller than 1kvm")
+//    @Column(name = "room_size", nullable = false)
+    private int roomSize;
     @OneToMany(mappedBy = "room")
     private List<Booking> bookings;
 
-    public Room(String roomNumber, String roomName, int size, List<Booking> bookings) {
+    public Room(String roomNumber, String roomName, int roomSize, List<Booking> bookings) {
         this.roomNumber = roomNumber;
         this.roomName = roomName;
-        this.size = size;
+        this.roomSize = roomSize;
         this.bookings = bookings;
     }
 }
--- src/main/java/backend1/bookingprogram/service/RoomTypeService.java
+++ src/main/java/backend1/bookingprogram/service/RoomTypeService.java
@@ -16,7 +16,7 @@ public class RoomTypeService {
 
     // room size determines type of room. //TODO return type or something later for business logic in booking.
     public String classifyRoom(Long roomId) {
-        return roomRepository.findById(roomId).map(room -> switch (getRoomType(room.getSize())) {
+        return roomRepository.findById(roomId).map(room -> switch (getRoomType(room.getRoomSize())) {
             case 1 -> "Single Room (1 bed)";
             case 2 -> "Double room (1-2 beds)";
             default -> "Suite (up to 3 beds)";
