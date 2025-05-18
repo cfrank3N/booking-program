43d17971134c72a97b5e822026ec9ea63f98869d
Gaurgle <justwannagroove@gmail.com>
2025-05-18 17:29:29 +0200

    room type switch check

--- src/main/java/backend1/bookingprogram/service/RoomTypeService.java
+++ src/main/java/backend1/bookingprogram/service/RoomTypeService.java
@@ -4,7 +4,6 @@ import backend1.bookingprogram.models.Room;
 import backend1.bookingprogram.repositories.RoomRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class RoomTypeService {
@@ -15,9 +14,18 @@ public class RoomTypeService {
         this.roomRepository = roomRepository;
     }
 
+    // room size determines type of room. //TODO return type or something later for business logic in booking.
     public String classifyRoom(Long roomId) {
-        return roomRepository.findById(roomId).map(room -> switch (getRoomTypeCode(room.getSize())) {
-            case 1 ->
-        }
+        return roomRepository.findById(roomId).map(room -> switch (getRoomType(room.getSize())) {
+            case 1 -> "Single Room (1 bed)";
+            case 2 -> "Double room (1-2 beds)";
+            default -> "Suite (up to 3 beds)";
+        }).orElse("Room not found");
+    }
+
+    private int getRoomType(int size) {
+        if (size < 20) return 1;
+        else if (size < 40) return 2;
+        else return 3;
     }
 }
