fda2930b074f170f608be3e94ed2430343d3b638
cfrank3N <adamcecarfrank@gmail.com>
2025-05-21 20:12:41 +0200

    Created a homepage with a simple form input=date

--- src/main/java/backend1/bookingprogram/controllers/RoomController.java
+++ src/main/java/backend1/bookingprogram/controllers/RoomController.java
@@ -4,6 +4,8 @@ import backend1.bookingprogram.dtos.RoomDTO;
 import backend1.bookingprogram.repositories.RoomRepository;
 import backend1.bookingprogram.service.RoomService;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import java.util.List;
 
 import static backend1.bookingprogram.mappers.RoomMapper.roomToRoomDTODetailed;
 
-@RestController
+@Controller
 public class RoomController {
 
     private RoomService roomService;
@@ -22,8 +24,9 @@ public class RoomController {
     }
 
     @GetMapping("/rooms")
-    public List<RoomDTO> fetchAllRooms() {
-        return roomService.fetchAllRooms();
+    public String fetchAllRooms(Model model) {
+        //List<RoomDTO> rooms = roomService.fetchAllRooms();
+        //model.addAttribute("rooms", rooms);
+        return "available-rooms";
     }
-
 }
