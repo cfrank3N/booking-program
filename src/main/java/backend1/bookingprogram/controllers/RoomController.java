ac48b9a341880a77b54783878658d6010cebc242
cfrank3N <adamcecarfrank@gmail.com>
2025-05-21 21:08:51 +0200

    Changed request from get to put to get rid of the parameters in the url

--- src/main/java/backend1/bookingprogram/controllers/RoomController.java
+++ src/main/java/backend1/bookingprogram/controllers/RoomController.java
@@ -1,22 +1,13 @@
 package backend1.bookingprogram.controllers;
 
-import backend1.bookingprogram.dtos.RoomDTO;
 import backend1.bookingprogram.dtos.RoomSearchDTO;
-import backend1.bookingprogram.repositories.RoomRepository;
 import backend1.bookingprogram.service.RoomService;
 import org.slf4j.LoggerFactory;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
-
-import static backend1.bookingprogram.mappers.RoomMapper.roomToRoomDTODetailed;
 
 @Controller
 public class RoomController {
@@ -28,7 +19,7 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    @GetMapping("/rooms")
+    @PostMapping("/rooms")
     public String fetchAllRooms(@ModelAttribute RoomSearchDTO r,
                                 Model model) {
         //List<RoomDTO> rooms = roomService.fetchAllRooms();
