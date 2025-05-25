569e9039ceb6e1ef3191aab7b0bfafe3c4074db3
cfrank3N <adamcecarfrank@gmail.com>
2025-05-25 21:53:25 +0200

    GuestController test on registerGuest(...) complete

---
+++ src/test/java/backend1/bookingprogram/controllers/GuestControllerTest.java
@@ -0,0 +1,66 @@
+package backend1.bookingprogram.controllers;
+
+import backend1.bookingprogram.models.Guest;
+import backend1.bookingprogram.repositories.GuestRepository;
+import backend1.bookingprogram.service.GuestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static backend1.bookingprogram.enums.RoutingInfo.REGISTER_GUEST;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static backend1.bookingprogram.enums.RoutingInfo.SELECT_ROOM;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class GuestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private GuestRepository repo;
+
+    @Test
+    void registerGuest() throws Exception {
+
+        mvc.perform(post("/guest/register")
+                .param("name", "Caroline")
+                .param("email", "test@email.com")
+                .param("phonenumber", "073 074 74 74"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(REGISTER_GUEST.getViewName()))
+                .andExpect(model().attributeExists("guest"))
+                .andExpect(model().attributeExists("success"));
+
+        Guest g = repo.findByEmail("test@email.com").orElse(new Guest());
+        repo.deleteById(g.getId());
+
+        //Test to see if an error is thrown
+        mvc.perform(post("/guest/register")
+                        .param("name", "Caroline")
+                        .param("email", "Andreas@Hotmale.com")
+                        .param("phonenumber", "073 074 74 74"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(REGISTER_GUEST.getViewName()))
+                .andExpect(model().attributeExists("guest"))
+                .andExpect(model().attributeExists("error"));
+    }
+}
