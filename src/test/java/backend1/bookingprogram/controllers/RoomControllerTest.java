ab16344f5e26a009c3e95788a08704a1b05d7e04
cfrank3N <adamcecarfrank@gmail.com>
2025-05-25 21:03:35 +0200

    Removed unused imports

--- src/test/java/backend1/bookingprogram/controllers/RoomControllerTest.java
+++ src/test/java/backend1/bookingprogram/controllers/RoomControllerTest.java
@@ -1,8 +1,5 @@
 package backend1.bookingprogram.controllers;
 
-import backend1.bookingprogram.dtos.BookingDTO;
-import backend1.bookingprogram.dtos.RoomDTO;
-import backend1.bookingprogram.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,15 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import static backend1.bookingprogram.enums.RoutingInfo.SELECT_ROOM;
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -30,9 +20,6 @@ class RoomControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private RoomService roomService;
 
     @Test
     void fetchAllAvailableRooms() throws Exception {
@@ -58,7 +45,7 @@ class RoomControllerTest {
         LocalDate endDate = LocalDate.now().plusDays(2);
         int numberOfGuests = 2;
 
-        //Make sure you have an initialized mockdb with data and that it has a booking with id 1.
+        //Make sure you have an initialized mock db with data and that it has a booking with id 1.
         //If it doesn't you can just look up the id and exchange it below in the uri in the post() method.
         mvc.perform(post("/booking/alter/1/rooms")
                         .param("dateFrom", startDate.toString())
